95b7ac9e383d642bb249f864d8f5744c4e505e6c
geruk <geruk@geruk-AMZNSEA.(none)>
2012-09-03 08:09:42 -0400

    Remember: Using Class.getResource() to get any kind of files!!!

--- src/edu/drexel/cs451/hangman/WordAccessor.java
+++ src/edu/drexel/cs451/hangman/WordAccessor.java
@@ -7,8 +7,6 @@
 
 package edu.drexel.cs451.hangman;
 
-
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
@@ -17,49 +15,42 @@ import java.util.Scanner;
 
 public class WordAccessor implements ProvidingWord {
     private static String filename = "HangmanWords.txt";
-	private final String DictionaryFile;
-	private List<String> dictionary;
-	private static WordAccessor singleInstance = null;
-	
-	public static WordAccessor getInstance() {
-	    if (singleInstance == null)
-	        singleInstance = new WordAccessor();
-	    return singleInstance;
-	}
-	
-	private WordAccessor() {
-	    this(filename);
-	}
-	
-	private WordAccessor(String filename)
-	{
-		this.DictionaryFile = filename;
-	}
-	
-	public void loadDictionary()
-	{
-	 // open the dictionary file and read dictionary into an ArrayList
-		Scanner input;
-		try {
-			input = new Scanner(new File(DictionaryFile));
-			dictionary = new ArrayList<String>();
-			while (input.hasNext())
-				dictionary.add(input.next().toLowerCase());
-			input.close();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
+    private final String DictionaryFile;
+    private List<String> dictionary;
+    private static WordAccessor singleInstance = null;
 
-	}
-	
-	public String getRandomWord()
-	{
-		Random randomGenerator = new Random();
-		int randomInt = randomGenerator.nextInt(dictionary.size());
-		
-		return dictionary.get(randomInt);
-		
-	}
-	
+    public static WordAccessor getInstance() {
+        if (singleInstance == null)
+            singleInstance = new WordAccessor();
+        return singleInstance;
+    }
+
+    private WordAccessor() {
+        this(filename);
+    }
+
+    private WordAccessor(String filename) {
+        this.DictionaryFile = filename;
+        loadDictionary();
+    }
+
+    private void loadDictionary() {
+        // open the dictionary file and read dictionary into an ArrayList
+        Scanner input;
+        input = new Scanner(this.getClass().getResourceAsStream(DictionaryFile));
+        dictionary = new ArrayList<String>();
+        while (input.hasNext())
+            dictionary.add(input.next().toLowerCase());
+        input.close();
+
+    }
+
+    public String getRandomWord() {
+        Random randomGenerator = new Random();
+        int randomInt = randomGenerator.nextInt(dictionary.size());
+
+        return dictionary.get(randomInt);
+
+    }
 
 }
